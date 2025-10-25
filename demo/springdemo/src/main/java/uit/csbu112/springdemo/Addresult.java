49dae6562d24a37eb3f0298585b79e8ef547b189
Nguyễn Thành Phúc <phucabc987@gmail.com>
2025-10-25 11:13:46 +0700

    Update add result

---
+++ demo/springdemo/src/main/java/uit/csbu112/springdemo/Addresult.java
@@ -0,0 +1,13 @@
+package uit.csbu112.springdemo;
+
+public class Addresult {
+    private int result;
+
+    public Addresult(int result) {
+        this.result = result;
+    }
+
+    public int getResult() {
+        return result;
+    }
+}
--- demo/springdemo/src/main/java/uit/csbu112/springdemo/HelloController.java
+++ demo/springdemo/src/main/java/uit/csbu112/springdemo/HelloController.java
@@ -1,13 +1,24 @@
 package uit.csbu112.springdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello world!";
+    public Student sayHello() {
+        return new Student("Nguyễn Thành Phúc", 24560045);
     }
+    @GetMapping("/add")
+    public Addresult add(@RequestParam int a, @RequestParam int b) {
+        return new Addresult(a+b);
+    }
+
+    // public int add(@RequestParam int a, @RequestParam int b) {
+    //     return (a+b);
+    // }
+    
 }
