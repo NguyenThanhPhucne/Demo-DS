01be4abb8bd9a4e1701bfde443c612c75d7cc7ce
Nguyễn Thành Phúc <phucabc987@gmail.com>
2025-10-25 10:50:59 +0700

    Update file springboot

---
+++ demo/springdemo/src/main/java/uit/csbu112/springdemo/HelloController.java
@@ -0,0 +1,13 @@
+package uit.csbu112.springdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ demo/springdemo/src/test/java/uit/csbu112/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uit.csbu112.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
