8ebed241d29bdeb22d89e63df40e00e5a571e118
Nguyễn Thành Phúc <phucabc987@gmail.com>
2025-10-31 17:54:26 +0700

    Update HelloController.java

--- demo/springdemo/src/main/java/uit/csbu112/springdemo/HelloController.java
+++ demo/springdemo/src/main/java/uit/csbu112/springdemo/HelloController.java
@@ -11,12 +11,13 @@ public class HelloController {
     @GetMapping("/hello")
     public Student sayHello() {
         return new Student("Nguyễn Thành Phúc", 24560045);
+        // http://localhost:8081/hello
     }
     @GetMapping("/add")
     public Addresult add(@RequestParam int a, @RequestParam int b) {
         return new Addresult(a+b);
     }
-
+        // http://localhost:8081/add?a=1&b=2
     // public int add(@RequestParam int a, @RequestParam int b) {
     //     return (a+b);
     // }
